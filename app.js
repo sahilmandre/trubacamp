2ca62355167b7ac154c253bf0c4aa4bf3719044a
sahilmandre <sahilmandre@gmail.com>
2021-04-28 12:26:39 +0530

    atlas added

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes   = require("./routes/comments");
     indexRoutes      = require("./routes/index")
 
 // mongodb://localhost:27017/Yelp_camp_v12
-var url = process.env.DATABASEURL   || "mongodb+srv://Sam123:<Sam123>@cluster0.s7kr8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+var url = process.env.DATABASEURL   || "mongodb+srv://Sam123:Sam123@cluster0.s7kr8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
