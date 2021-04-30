5c4893bd3c9525aa3e8acc349d9796e041191666
sahilmandre <sahilmandre@gmail.com>
2021-04-30 11:54:41 +0530

    making it better

--- app.js
+++ app.js
@@ -59,10 +59,10 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000 , function () {
-    console.log("Truba Camp server started");
-});
-
-// app.listen(process.env.PORT , process.env.IP,  function () {
+// app.listen(3000 , function () {
 //     console.log("Truba Camp server started");
 // });
+
+app.listen(process.env.PORT , process.env.IP,  function () {
+    console.log("Truba Camp server started");
+});
